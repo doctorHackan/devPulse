79e74859a18274b8667c60be0e4a9d7e7f202a2e
Drubo Roy Bishwas <doctorhackan@gmail.com>
2026-05-24 22:32:16 +0600

    added global error handler

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,20 @@
 import express, { type Request, type Response } from "express";
 import { authRouter } from "./modules/auth/auth.route";
 import { issueRoute } from "./modules/issue/issue.route";
+import cors from "cors";
+import CookieParser from "cookie-parser";
+import globalErrorHandler from "./middleware/globalErroHandler";
 
 const app = express();
 
 app.use(express.json());
+app.use(CookieParser());
 
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  }),
+);
 
 
 app.get("/",(req: Request, res : Response)=>{
@@ -18,6 +27,8 @@ app.get("/",(req: Request, res : Response)=>{
 
 app.use("/api/auth",authRouter);
 app.use("/api/issue",issueRoute);
+
+app.use(globalErrorHandler);
 
 
 export default app;
